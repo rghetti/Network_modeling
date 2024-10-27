ef9a42e3cfa69a7930f38d88951557d03ff09d68
eakravchenko <kravchenkoea02@gmail.com>
2024-10-27 22:42:45 +0100

    implemented R code for 1.2-1.3

--- Task1.R
+++ Task1.R
@@ -7,7 +7,7 @@ library(network)
 
 #importing the data
 
-setwd("C:/Users/Riccardo Ghetti/Downloads/Network modeling/assignment1/assignment1/Krackhardt-High-Tech_Multiplex_Social/Dataset")
+setwd("./Dataset")
 list.files()
 attributes=read.table("Krackhardt-High-Tech_nodes.txt",header=TRUE)
 nodes=as.matrix(read.table("Krackhardt-High-Tech_multiplex.edges",header=FALSE))
@@ -38,6 +38,57 @@ for (k in 1:190){
 g1=netlogit(ad.matrix,f.matrix, rep=permutations,nullhyp='qapy')
 g1$names=c('intercept','friendship')
 summary(g1)
+
+
+g1.5=netlogit(f.matrix,ad.matrix, rep=permutations,nullhyp='qapy')
+g1.5$names=c('intercept', 'advice')
+summary(g1.5)
 #at the significance level of 0.05 we can see there is statistical 
 #evidence that friendship is a good predictor of advice seeking
+
+
+#extract same_department as a bool matrix (1 is departments are the same and 0 otherwise)
+
+num_people = 21
+
+same_department.matrix=matrix(0, nrow=num_people, ncol=num_people)
+for (i in 1:num_people){
+  for (j in 1:num_people){
+    if(attributes[["nodeDepartment"]][i] == attributes[["nodeDepartment"]][j])
+      same_department.matrix[i, j] = 1
+  }
+}
+
+#make a matrix for testing that senior managers are less likely to nominate friends. The value at position (i, j) is tenure[i]
+
+tenure.matrix = matrix(0, nrow=num_people, ncol=num_people)
+for(i in 1:num_people){
+  for(j in 1:num_people){
+    tenure.matrix[i, j] = attributes[["nodeTenure"]][i] 
+  }
+}
+
+#make a matrix for testing that a friendship nomination is more likely between a pair of managers of a similar age. The value at position (i, j) is |age[i] - age[j]|
+
+
+age_dif_matrix = matrix(0, nrow=num_people, ncol=num_people)
+for(i in 1:num_people){
+  for(j in 1:num_people){
+    age_dif_matrix[i, j] = abs(attributes[["nodeAge"]][i]-attributes[["nodeAge"]][j])
+  }
+}
+
+#build the QAP test with additional features
+
+zm <- list(ad.matrix, same_department.matrix, tenure.matrix, age_dif_matrix)
+
+g2<-netlogit(f.matrix,zm, rep=permutations,nullhyp='qapy')
+g2$names<-c('intercept', 'advice', 'same_department', 'tenure', 'age_dif')
+summary(g2)
+
+#at the significance level of 0.05 we can see there is statistical 
+#evidence that people more likely nominate their friends peoples from the same 
+#department or people, who they are asking an advice.
+#From the other hand, at the significance level of 0.05, tenure and 
+#age difference do not correlate with friendship nomination
 
